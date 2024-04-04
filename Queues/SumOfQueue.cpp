2076356d6d0f1299028582ff5b9c2291b849d84c
Arham Malik <arhammalik900@yahoo.com>
2024-04-04 10:33:12 +0500

    Add files via upload

---
+++ Queues/SumOfQueue.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+#define n 100
+
+class queue
+{
+    int *arr;
+    int front;
+    int back;
+
+public:
+    // constructor
+    queue()
+    {
+        arr = new int[n];
+        front = -1;
+        back = -1;
+    }
+
+    // pushing
+    void enqueue(int x)
+    {
+        if (back == n - 1)
+        {
+            cout << "Queue Overflow" << endl;
+            return;
+        }
+        back++;
+        arr[back] = x; 
+        if (front == -1)
+        {
+            front++;
+        }
+    }
+   //Display Sum Of Queue
+   void display()
+    {
+        if (front == -1)
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        int sum=0;
+        for (int i = front; i <= back; i++)
+        {
+            sum+= arr[i];
+        }
+        cout<<sum << endl;
+    }
+};
+
+int main()
+{
+    queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.display();
+    return 0;
+}
